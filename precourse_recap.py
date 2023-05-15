143a539eadbaa7dd47930230c371f7d1e24ae0ce
Mark Slorach <hello@markslorach.com>
2023-05-15 16:05:33 +0100

    wrote some python code

---
+++ precourse_recap.py
@@ -0,0 +1,16 @@
+# adding two numbers / converting string to int
+num_1 = 5
+num_2 = "5"
+
+print(num_1 + int(num_2))
+
+# adding strings / capitalize function / converting int to string
+name = "mark"
+age = 31
+
+print("Hello I am " + name.capitalize() + ". " + "I am " + str(age) + " years old.")
+
+
+
+
+
